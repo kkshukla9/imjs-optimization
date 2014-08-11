fa15d1296bca248a6726767dec50b7af5c4d9ea6
krishna kant shukla <krishna.shukla@indiamart.com>
2014-08-11 11:18:30 +0530

    few code added in function

--- checkdataSearch.js
+++ checkdataSearch.js
@@ -1,8 +1,17 @@
 function rol(p1,p2,p3,p4,p5){recordOutboundLink(p1,p2,p3,p4,p5);}
+function refSrchStr(f,s,ts,pos,pu,key,rolkey){
+			s=key+'='+escape(s);
+			s=pu+s;
+			s=myReplace(s,"\\\\?\\\\&","?");
+			if(ts==1) rol(s,'Trac-AutoSuggest',pos,0,0);
+			rol(s,rolkey,pos,0,1);
+			window.location = s;
+            return false;
+}
 //wa=webAddress|f=Form
 function CheckDataSearch(f)
 {
-    var http='http://'; var ts=trackSuggest;
+    var http='http://'; var ts=trackSuggest;var url='';
 	var wa = http+location.hostname;
     var im='indiamart.com';var dirfl='/cgi/catprdsearch.mp?';var trdfl='/search.mp?';var buyfl='/buyersearch.mp?';
     var pos=(f.name=='searchForm')?'Search-Top':(f.name=='searchForm1')?'Search-Bottom':'Search-Top';
@@ -14,7 +23,7 @@ function CheckDataSearch(f)
 		f.ss.focus();
 		return false;
 	}
-	if (f.ss.value.replace(/\s/g, '').length < 1){
+	if (f.ss.value.replace(/\s/g,'').length < 1){
 		alert("Please enter text to search.");
 		f.ss.focus();
 		return false;
@@ -26,57 +35,27 @@ function CheckDataSearch(f)
 		}
 		var str = f.ss.value.replace(/\s|%20/g, '+');
 		if(f.txv.value == "Suppliers"){
-			str ='ss='+escape(str);
-			var page_url= wa.match(/dir\.i/) ? wa + dirfl : http+"dir."+im+dirfl;
-			str = page_url+str;
-			str = myReplace(str,"\\\\?\\\\&","?");
-			if (ts == 1) rol(str, 'Trac-AutoSuggest', pos,0,0);
-			rol(str, 'Trac-Search', pos,0,1);
-			window.location = str;
-			return false;
+			url= wa.match(/dir\.i/) ? wa + dirfl : http+"dir."+im+dirfl;
+            return refSrchStr(f,str,ts,pos,url,'ss','Trac-Search');
 		}else if(f.txv.value == "Products" || f.txv.value == "products"){
-			str ='ss='+escape(str);
-			var page_url= wa.match(/dir\.i/) ?  wa + dirfl : http+"dir."+im+dirfl;
-			str = page_url +str;
-			str = myReplace(str,"\\\\?\\\\&","?");
-			if (ts == 1)	rol(str, 'Trac-AutoSuggest', pos,0,0);
-			rol(str, 'Trac-Search', pos,0,1);
-			window.location = str;
-			return false;
+			url= wa.match(/dir\.i/) ?  wa+dirfl : http+"dir."+im+dirfl;
+            return refSrchStr(f,str,ts,pos,url,'ss','Trac-Search');
 		}else if(f.txv.value == "Sell Offers"){
+			url= wa.match(/trade\.i/)? wa+trdfl : http+"trade."+im+trdfl;
 			f.search.value = str;
-			str ='search='+escape(str);
-			str = myReplace(str,"\\\\?\\\\&","?");
-			var page_url= wa.match(/trade\.i/)? wa +trdfl : http+"trade."+im+trdfl;
-			var urlStr = page_url+str;
-			window.location = urlStr;
-			if (ts == 1) rol(urlStr, 'Trac-AutoSuggest', pos,0,1);
-			rol(urlStr, 'ETO', pos,0,0);
-			return false;
+            return refSrchStr(f,str,ts,pos,url,'search','ETO');
 		}else if(f.txv.value == "Tenders"){
-			str ='ss='+escape(str);
-			str = http+"tenders."+im+"/search.cgi?"+str;
-			str = myReplace(str,"\\\\?\\\\&","?");
-			if (ts == 1) rol(str, 'Trac-AutoSuggest', pos,0,0);
-			rol(str, 'Tenders', pos,0,1);
-			window.location = str;
-			return false;
+            url=http+"tenders."+im+"/search.cgi?";
+            return refSrchStr(f,str,ts,pos,url,'search','Tenders');
 		}else if(f.txv.value == "Buy Leads"){
-			var param_city = (f.city && f.city.value) ? '&city='+escape(f.city.value) : '';
-			str ='search='+escape(str);
-			str = myReplace(str,"\\\\?\\\\&","?");
-			var page_url= (wa.match(/dev-/)) ? http+"dev-trade."+im+buyfl : (wa.match(/stg-/)) ? http+"stg-trade."+im+buyfl : http+"trade."+im+buyfl;
-			var urlStr = page_url+str+param_city;
-			if(ts == 1) rol(urlStr, 'Trac-AutoSuggest', pos,0,1);
-			if(f.tradeSearch && f.tradeSearch.value == 1)
-			{
-				rol(urlStr, 'ETO', 'BL-Tab',0,0);
+			url= (wa.match(/dev-/)) ? http+"dev-trade."+im+buyfl : (wa.match(/stg-/)) ? http+"stg-trade."+im+buyfl : http+"trade."+im+buyfl;
+			var city = (f.city && f.city.value) ? '&city='+escape(f.city.value) : '';
+            str=str+city;
+			if(f.tradeSearch && f.tradeSearch.value == 1){
+                pos='BL-Tab';
 				f.tradeSearch.value = '';
-			}else{
-                rol(urlStr, 'ETO', pos,0,0);
 			}
-			window.location = urlStr;
-			return false;
+            return refSrchStr(f,str,ts,pos,url,'search','ETO');
 		}
 	}
 }
